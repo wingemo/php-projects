dfdecb2a4b15f09bdf1bd45a14cd5c7ef5ab2cd7
Wingemo <philip.wingemo@outlook.com>
2021-08-22 20:10:09 +0200

    Update Controller.php

--- Fortnox/Controller.php
+++ Fortnox/Controller.php
@@ -1,17 +1,18 @@
 <?php
 
-foreach ($arr as &$value) {
-    // List all accounts and sum togheter
-    function accounts_sum() {
-      $accounts = apiCall( 'GET', 'accounts');
-      return $accounts;
-    }
+// List all accounts and sum togheter
+function accounts_sum() {
+  $accounts = apiCall( 'GET', 'accounts');
+  return $accounts;
+}
 
-    // Retrieve a list of orders and sum together
-    function orders_sum() {
-      $orders = apiCall( 'GET', 'orders');
-      return $orders;
-    }
+// Retrieve a list of orders and sum together
+function orders_sum() {
+  $orders = apiCall( 'GET', 'orders');
+  return $orders;
+}
+
+foreach ($arr as &$value) {
 
     // Send outgoing SMS message
     $message = accounts_sum(). orders_sum();
